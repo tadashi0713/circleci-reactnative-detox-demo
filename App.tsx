15f6fb9ac6ee9f1634bf35e7da8bc004c81195aa
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-08-11 02:24:19 +0900

    Fix lint

--- App.tsx
+++ App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export type Props = {
   name: string;
   baseEnthusiasmLevel?: number;
 };
 
-const App: React.FC<Props> = ({
-  name,
-  baseEnthusiasmLevel = 0
-}) => {
-  const [enthusiasmLevel, setEnthusiasmLevel] = React.useState(
-    baseEnthusiasmLevel
-  );
+const App: React.FC<Props> = ({name, baseEnthusiasmLevel = 0}) => {
+  const [enthusiasmLevel, setEnthusiasmLevel] =
+    React.useState(baseEnthusiasmLevel);
 
-  const onIncrement = () =>
-    setEnthusiasmLevel(enthusiasmLevel + 1);
+  const onIncrement = () => setEnthusiasmLevel(enthusiasmLevel + 1);
   const onDecrement = () =>
-    setEnthusiasmLevel(
-      enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0
-    );
+    setEnthusiasmLevel(enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0);
 
   const getExclamationMarks = (numChars: number) =>
     numChars > 0 ? Array(numChars + 1).join('!') : '';
@@ -52,13 +45,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   greeting: {
     fontSize: 20,
     fontWeight: 'bold',
-    margin: 16
-  }
+    margin: 16,
+  },
 });
 
 export default App;
